2881c4b68caae5b41b192e471f4ed5fe68e9f6aa
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-16 16:58:08 -0300

    feat(types/cars): cars type

---
+++ types/cars.ts
@@ -0,0 +1,10 @@
+export type CARS = {
+    id?: string;
+    image?: string | null;
+    model: string;
+    year: number;
+    name: string;
+    description: string;
+    price: string; 
+    mileage: string;
+}
